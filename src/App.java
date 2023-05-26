6b8cafdccd543e2caf66ad66e7beec32fdca22da
Cezar Augusto Mezzalira <cezar.mezzalira@gmail.com>
2023-05-25 21:50:49 -0300

    Terceiro commit

--- src/App.java
+++ src/App.java
@@ -1,6 +1,7 @@
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("App.main()");
+
+        System.out.println("Apenas que busquem conhecimento. Bilu.");
     }
 }
